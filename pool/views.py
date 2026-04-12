1eea81d30ce9a20e11db0de64514dbaf84e86b59
Emycyber <emycyber@gmail.com>
2026-04-12 00:02:09 -0700

    Fix sitemap and add robots.txt

--- pool/views.py
+++ pool/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 from django.db.models import F
 from django.contrib.auth.models import User
 from blog.models import BlogPage
