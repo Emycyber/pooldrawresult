b3671f37da756ead82ceb1aaabd7e020a7aa6e89
Emycyber <emycyber@gmail.com>
2026-04-16 10:17:39 -0700

    Fix wagtail_hooks format_html error

--- blog/wagtail_hooks.py
+++ blog/wagtail_hooks.py
@@ -1,18 +1,24 @@
 from wagtail import hooks
 from wagtail.admin import messages
-from django.utils.html import format_html
+from django.shortcuts import redirect
 
 
 @hooks.register('before_delete_page')
 def before_delete_page(request, page):
-    from blog.models import BlogIndexPage
+    from blog.models import BlogIndexPage, BlogPage
+
+    # Block deletion of Blog index page
     if isinstance(page, BlogIndexPage):
         messages.error(
             request,
-            format_html(
-                '<b>Cannot delete the Blog index page.</b> '
-                'Unpublish it instead if you want to hide it.'
-            )
+            'Cannot delete the Blog index page. Unpublish it instead if you want to hide it.'
         )
-        from django.shortcuts import redirect
         return redirect('wagtailadmin_explore', page.get_parent().id)
+
+    # For blog posts clear related objects before deletion
+    if isinstance(page, BlogPage):
+        try:
+            page.tagged_items.all().delete()
+            page.categories.clear()
+        except Exception:
+            pass
